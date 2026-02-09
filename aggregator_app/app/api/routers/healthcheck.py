4e11d6122ff3be2481e1c5c765054eb53d170b52
Vyacheslav <vyvelger@yandex.ru>
2026-02-09 16:50:34 +0700

    chore: добавлен тег к health

--- aggregator_app/app/api/routers/healthcheck.py
+++ aggregator_app/app/api/routers/healthcheck.py
@@ -5,7 +5,7 @@ from fastapi import APIRouter
 router = APIRouter()
 
 
-@router.get("/health")
+@router.get("/health", tags=["healthcheck"])
 async def healthcheck():
     """Проверить работоспособность API."""
     return {"status": "ok"}
