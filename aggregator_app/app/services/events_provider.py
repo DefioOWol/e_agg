b89d1998a18bbdd0b8194ff650cbf0ca6c6e224f
Vyacheslav <vyvelger@yandex.ru>
2026-02-10 01:59:22 +0700

    feat: добавлен модуль взаимодействия с EventsProviderAPI

---
+++ aggregator_app/app/services/events_provider.py
@@ -0,0 +1,77 @@
+"""Модуль взаимодействия с EventsProviderAPI."""
+
+from collections.abc import Any
+from datetime import date
+
+from aiohttp import ClientSession, ClientTimeout
+
+from app.config import settings
+
+
+class EventsProviderClient:
+    """Клиент для взаимодействия с EventsProviderAPI."""
+
+    BASE_URL = "http://events-provider.dev-1.python-labs.ru"
+
+    def __init__(self, total_timeout: int = 10, connect_timeout: int = 5):
+        """Инициализировать клиент."""
+        timeout = ClientTimeout(total=total_timeout, connect=connect_timeout)
+        self._session = ClientSession(
+            self.BASE_URL,
+            headers={"x-api-key": settings.lms_api_key.get_secret_value()},
+            timeout=timeout,
+            raise_for_status=True,
+        )
+
+    async def get_events(
+        self, changed_at: date, cursor: str | None = None
+    ) -> dict[str, Any]:
+        """Получить события."""
+        url = f"/api/events/?changed_at={changed_at.isoformat()}"
+        if cursor:
+            url += f"&cursor={cursor}"
+        async with self._session.get(url) as response:
+            return await response.json()
+
+    def close(self):
+        """Закрыть сессию."""
+        self._session.close()
+
+    @staticmethod
+    def extract_cursor(response: dict[str, Any]) -> str | None:
+        """Извлечь курсор из ответа."""
+        if (cursor := response.get("next")) is not None:
+            cursor = cursor.rsplit("cursor=", 1)[1]
+        return cursor
+
+
+class EventsPaginator:
+    """Пагинатор событий EventsProviderAPI."""
+
+    def __init__(self, client: EventsProviderClient, changed_at: date):
+        """Инициализировать пагинатор."""
+        self._client = client
+        self._changed_at = changed_at
+        self._cursor = None
+        self._events = []
+        self._current = 0
+
+    def __aiter__(self):
+        """Получить итератор событий."""
+        return self
+
+    async def __anext__(self) -> dict[str, Any]:
+        """Получить следующее событие."""
+        end_status = self._current >= len(self._events)
+        if self._cursor is None and end_status and self._current:
+            raise StopAsyncIteration
+        if end_status:
+            response = await self._client.get_events(
+                self._changed_at, self._cursor
+            )
+            self._cursor = self._client.extract_cursor(response)
+            self._events = response["results"]
+            self._current = 0
+        event = self._events[self._current]
+        self._current += 1
+        return event
