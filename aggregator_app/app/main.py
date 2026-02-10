182f7729774af116098ca5b4b561e4a88f8f9cbc
Vyacheslav <vyvelger@yandex.ru>
2026-02-10 20:08:11 +0700

    feat: добавлен lifespan для приложения

--- aggregator_app/app/main.py
+++ aggregator_app/app/main.py
@@ -1,13 +1,41 @@
 """Основной модуль приложения."""
 
+from contextlib import asynccontextmanager
+from datetime import UTC
+
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from fastapi import FastAPI
 
 from app.api.routers import healthcheck
+from app.orm.db_manager import db_manager
+from app.services import SyncService
+
+scheduler = AsyncIOScheduler(timezone=UTC)
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """Жизненный цикл приложения.
+
+    Инициализирует сессию базы данных и сервис синхронизации
+    и запускает планировщик.
+    В конце жизненного цикла закрывает сессию базы данных
+    и останавливает планировщик.
+
+    """
+    await db_manager.init()
+    await SyncService(scheduler).init_job()
+    scheduler.start()
+    yield
+    scheduler.shutdown()
+    await db_manager.close()
+
 
 app = FastAPI(
     title="Events aggregator API",
     description="API for events aggregator",
     version="1.0.0",
+    lifespan=lifespan,
     root_path="/api",
 )
 
