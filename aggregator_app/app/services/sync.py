8dda6d243ee09542927107754f1c5d6053289cf8
Vyacheslav <vyvelger@yandex.ru>
2026-02-10 23:44:29 +0700

    fix: добавлено логирование, исправлено добавление дубликатов и преобразование типов в SyncService

--- aggregator_app/app/services/sync.py
+++ aggregator_app/app/services/sync.py
@@ -1,9 +1,9 @@
 """Сервис синхронизации данных."""
 
+import logging
 from datetime import UTC, date, datetime
 from typing import Any
 
-from aiohttp import ClientTimeout
 from aiohttp.client_exceptions import ClientResponseError
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.triggers.interval import IntervalTrigger
@@ -11,13 +11,16 @@ from sqlalchemy import DateTime
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.orm.db_manager import db_manager
-from app.orm.models import Base, Event, Place, SyncMeta, SyncStatus
+from app.orm.models import Base, Event, EventStatus, Place, SyncMeta, SyncStatus
 from app.orm.repositories import (
     EventRepository,
     PlaceRepository,
     SyncMetaRepository,
 )
 from app.services.events_provider import EventsPaginator, EventsProviderClient
+
+logging.basicConfig(level=logging.INFO, filename="app.log", filemode="w")
+logger = logging.getLogger(__name__)
 
 
 class SyncService:
@@ -34,9 +37,16 @@ class SyncService:
 
     async def init_job(self):
         """Инициализировать задачу синхронизации."""
+        logger.info("Инициализация задачи")
+
         async with db_manager.session() as session:
             sync_meta = await self._get_sync_meta(session)
+
             if sync_meta.sync_status == SyncStatus.PENDING:
+                logger.info(
+                    "Обнаружен статус %s, исправляем",
+                    sync_meta.sync_status.value,
+                )
                 sync_meta.sync_status = (
                     SyncStatus.NEVER
                     if sync_meta.last_sync_time is None
@@ -51,9 +61,11 @@ class SyncService:
             max_instances=1,
             next_run_time=self._get_next_run_time(sync_meta.last_sync_time),
         )
+        logger.info("Задача синхронизации добавлена в планировщик")
 
     def trigger_job(self):
         """Вызвать задачу синхронизации."""
+        logger.info("Запрошен внеплановый запуск задачи")
         self._scheduler.scheduled_job(
             self.SYNC_JOB_TRIGGER,
             id=self.SYNC_JOB_ID,
@@ -61,20 +73,23 @@ class SyncService:
             next_run_time=datetime.now(UTC),
         )
 
-    def _get_next_run_time(self, last_sync_time: datetime | None) -> datetime:
-        """Получить следующее время запуска."""
-        if last_sync_time is None:
-            return datetime.now(UTC)
-        return last_sync_time + self.SYNC_JOB_TRIGGER.interval
-
     async def _sync(self):
         """Синхронизировать данные."""
+        logger.info("Начало синхронизации")
+
         async with db_manager.session() as session:
             sync_meta = await self._get_sync_meta(session)
+
             if (sync_status := sync_meta.sync_status) == SyncStatus.PENDING:
+                logger.warning("Синхронизация уже выполняется, пропускаем")
                 return
+
             sync_meta.sync_status = SyncStatus.PENDING
             await session.commit()
+
+        logger.info(
+            "Статус синхронизации установлен в %s", sync_meta.sync_status.value
+        )
 
         try:
             (
@@ -82,43 +97,56 @@ class SyncService:
                 place_data_list,
                 latest_changed_at,
             ) = await self._run_fetch(sync_meta)
-        except (ClientTimeout, ClientResponseError):
+        except (TimeoutError, ClientResponseError) as e:
+            logger.exception(
+                "Ошибка при получении данных из API: [%d] %s",
+                e.status,
+                e.message,
+            )
             await self._rollback_sync_meta(sync_status)
         else:
             await self._update_db(
                 event_data_list, place_data_list, latest_changed_at
             )
 
-    async def _get_sync_meta(self, session: AsyncSession) -> SyncMeta:
-        """Получить метаданные синхронизации."""
-        sync_meta_repo = SyncMetaRepository(session)
-        sync_meta, _ = await sync_meta_repo.get_or_add(for_update=True)
-        return sync_meta
+        logger.info("Синхронизация завершена")
 
     async def _run_fetch(
         self, sync_meta: SyncMeta
     ) -> tuple[list[dict[str, Any]], list[dict[str, Any]], date]:
         """Загрузить данные из API."""
+        latest_changed_at = sync_meta.last_changed_at or self.DEFAULT_CHANGED_AT
+        logger.info(
+            "Получение данных из API, начиная с %s",
+            latest_changed_at.isoformat(),
+        )
+
         async with EventsProviderClient() as client:
-            latest_changed_at = (
-                sync_meta.last_changed_at or self.DEFAULT_CHANGED_AT
-            )
+            event_data_dict = {}
+            place_data_dict = {}
             paginator = EventsPaginator(client, latest_changed_at)
 
-            event_data_list = []
-            place_data_list = []
             async for event in paginator:
                 place = event.pop("place")
-                self._normalize_datetime(place, Place)
-                self._normalize_datetime(event, Event)
+                self._prepare_place(place)
+                self._prepare_event(event)
 
-                place_data_list.append(place)
-                event_data_list.append(event | {"place_id": place["id"]})
+                place_data_dict[place["id"]] = place
+                event_data_dict[event["id"]] = event | {"place_id": place["id"]}
                 latest_changed_at = max(
                     latest_changed_at,
                     event["changed_at"].date(),
                 )
 
+        logger.info(
+            "Получение завершено: events=%d, places=%d, latest_changed_at=%s",
+            len(event_data_dict),
+            len(place_data_dict),
+            latest_changed_at.isoformat(),
+        )
+
+        event_data_list = list(event_data_dict.values())
+        place_data_list = list(place_data_dict.values())
         return event_data_list, place_data_list, latest_changed_at
 
     async def _update_db(
@@ -128,6 +156,12 @@ class SyncService:
         latest_changed_at: date,
     ):
         """Обновить базу данных."""
+        logger.info(
+            "Обновление БД: events=%d, places=%d",
+            len(event_data_list),
+            len(place_data_list),
+        )
+
         async with db_manager.session() as session:
             event_repo = EventRepository(session)
             place_repo = PlaceRepository(session)
@@ -141,20 +175,52 @@ class SyncService:
                 sync_meta.last_sync_time = datetime.now(UTC)
                 sync_meta.last_changed_at = latest_changed_at
 
+            logger.info("Метаданные обновлены: %s", str(sync_meta))
+        logger.info("Обновление завершено")
+
     async def _rollback_sync_meta(self, prev_sync_status: SyncStatus):
         """Обработать ошибку синхронизации."""
+        logger.info("Откатываем метаданные")
+
         async with db_manager.session() as session:
             sync_meta = await self._get_sync_meta(session)
             sync_meta.sync_status = prev_sync_status
             await session.commit()
 
-    def _normalize_datetime(
-        self, data: dict[str, Any], cls_: type[Base]
-    ) -> dict[str, Any]:
-        """Нормализовать даты в данных."""
+        logger.info(
+            "Статус синхронизации возвращен к %s", prev_sync_status.value
+        )
+
+    async def _get_sync_meta(self, session: AsyncSession) -> SyncMeta:
+        """Получить метаданные синхронизации."""
+        logger.info("Получаем метаданные")
+
+        sync_meta_repo = SyncMetaRepository(session)
+        sync_meta, _ = await sync_meta_repo.get_or_add(for_update=True)
+
+        logger.info("Метаданные получены: %s", str(sync_meta))
+        return sync_meta
+
+    def _get_next_run_time(self, last_sync_time: datetime | None) -> datetime:
+        """Получить следующее время запуска."""
+        if last_sync_time is None:
+            return datetime.now(UTC)
+        return last_sync_time + self.SYNC_JOB_TRIGGER.interval
+
+    def _prepare_place(self, place_data: dict[str, Any]):
+        """Подготовить данные места проведения."""
+        self._normalize_datetime(place_data, Place)
+
+    def _prepare_event(self, event_data: dict[str, Any]):
+        """Подготовить данные события."""
+        del event_data["number_of_visitors"]
+        event_data["status"] = EventStatus(event_data["status"])
+        self._normalize_datetime(event_data, Event)
+
+    def _normalize_datetime(self, data: dict[str, Any], cls_: type[Base]):
+        """Нормализовать даты."""
         for c in cls_.__table__.c:
-            if isinstance(c.type, DateTime):
+            if isinstance(c.type, DateTime) and c.key in data:
                 data[c.key] = datetime.fromisoformat(data[c.key]).astimezone(
                     UTC
                 )
-        return data
