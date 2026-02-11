a9220ef4f140a6297de600e339a2bd7d4951e72a
Vyacheslav <vyvelger@yandex.ru>
2026-02-11 14:14:19 +0700

    feat: добавлены зависимости фильтрации и получения сессии

---
+++ aggregator_app/app/api/dependencies.py
@@ -0,0 +1,26 @@
+"""Зависимости API."""
+
+from collections.abc import AsyncGenerator
+from datetime import date
+
+from fastapi_filter.contrib.sqlalchemy import Filter
+from pydantic import Field
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.orm.db_manager import db_manager
+from app.orm.models import Event
+
+
+class EventFilter(Filter):
+    """Фильтр событий."""
+
+    event_time__gte: date | None = Field(alias="date_from")
+
+    class Constants(Filter.Constants):
+        model = Event
+
+
+async def get_session() -> AsyncGenerator[AsyncSession, None]:
+    """Получить сессию БД."""
+    async with db_manager.session() as session:
+        yield session
