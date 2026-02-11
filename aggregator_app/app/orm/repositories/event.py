7569f86bd2100d17456430cbc46763d5ae052114
Vyacheslav <vyvelger@yandex.ru>
2026-02-11 14:34:47 +0700

    feat: добавлены методы получения пагинации и скаляра в репозитории Event

--- aggregator_app/app/orm/repositories/event.py
+++ aggregator_app/app/orm/repositories/event.py
@@ -2,6 +2,7 @@
 
 from typing import Any
 
+from sqlalchemy import Select
 from sqlalchemy.dialects.postgresql import insert
 
 from app.orm.models import Event
@@ -11,7 +12,22 @@ from app.orm.repositories.base import BaseRepository
 class EventRepository(BaseRepository):
     """Репозиторий событий."""
 
+    async def get_paginated(
+        self, stmt: Select, page: int, page_size: int | None
+    ) -> list[Event]:
+        """Получить погинированные события по запросу."""
+        if page_size:
+            stmt = stmt.offset((page - 1) * page_size).limit(page_size)
+        result = await self._session.execute(stmt)
+        return result.scalars().all()
+
+    async def get_select_scalar(self, stmt: Select) -> Any:
+        """Получить скалярное значение по запросу."""
+        result = await self._session.execute(stmt)
+        return result.scalar_one()
+
     async def upsert(self, json_data_list: list[dict[str, Any]]):
+        """Вставить или обновить записи при конфликте."""
         stmt = insert(Event).values(json_data_list)
         stmt = stmt.on_conflict_do_update(
             index_elements=[Event.id],
