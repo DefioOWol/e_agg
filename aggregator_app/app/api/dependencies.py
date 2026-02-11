b87848fff485caa25dd8665593c1aac35e42857d
Vyacheslav <vyvelger@yandex.ru>
2026-02-11 21:24:11 +0700

    feat: добавлена проверка формата даты для фильтрации

--- aggregator_app/app/api/dependencies.py
+++ aggregator_app/app/api/dependencies.py
@@ -4,7 +4,7 @@ from collections.abc import AsyncGenerator
 from datetime import date
 
 from fastapi_filter.contrib.sqlalchemy import Filter
-from pydantic import Field
+from pydantic import Field, field_validator
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.orm.db_manager import db_manager
@@ -16,8 +16,17 @@ class EventFilter(Filter):
 
     event_time__gte: date | None = Field(None, alias="date_from")
 
+    @field_validator("event_time__gte")
+    def string_to_date(cls, v):
+        if isinstance(v, str):
+            return date.fromisoformat(v)
+        return v
+
     class Constants(Filter.Constants):
         model = Event
+
+    class Config:
+        validate_by_name = True
 
 
 async def get_session() -> AsyncGenerator[AsyncSession, None]:
