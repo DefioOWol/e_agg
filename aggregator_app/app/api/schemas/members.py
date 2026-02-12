4d9b5b0e1782aaf94c4c30feb6be121acb4fc444
Vyacheslav <vyvelger@yandex.ru>
2026-02-12 14:34:33 +0700

    feat: обновлена валидация полей участника

--- aggregator_app/app/api/schemas/members.py
+++ aggregator_app/app/api/schemas/members.py
@@ -9,7 +9,11 @@ class MemberIn(BaseModel):
     """Схема регистрации участника."""
 
     event_id: UUID
-    first_name: str = Field(min_length=2, max_length=32)
-    last_name: str = Field(min_length=2, max_length=32)
+    first_name: str = Field(
+        min_length=2, max_length=32, pattern=r"^[А-Я][а-я]+$"
+    )
+    last_name: str = Field(
+        min_length=2, max_length=32, pattern=r"^[А-Я][а-я]+$"
+    )
     email: EmailStr
-    seat: str = Field(pattern=r"^[A-Z][0-9]+$")
+    seat: str = Field(pattern=r"^[A-Z][1-9][0-9]{0,6}$")
