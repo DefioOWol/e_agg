c9bd944f6cc5fa8ef8a2bb26fcd18f88a236ecc0
Vyacheslav <vyvelger@yandex.ru>
2026-02-13 18:27:38 +0700

    feat: обновлена структура зависимостей api

--- aggregator_app/app/api/dependencies.py
+++ aggregator_app/app/api/dependencies.py
@@ -1,30 +1,40 @@
 """Зависимости API."""
 
-from collections.abc import AsyncGenerator
 from typing import Annotated
 
 from fastapi import Depends
-from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.orm.db_manager import db_manager
-from app.services import EventsProviderClient, EventsService, TicketsService
+from app.orm.uow import IUnitOfWork, SqlAlchemyUnitOfWork
+from app.services.events import EventsService
+from app.services.events_provider import (
+    EventsProviderClient,
+    IEventsProviderClient,
+)
+from app.services.tickets import TicketsService
 
 
-async def get_session() -> AsyncGenerator[AsyncSession, None]:
-    """Получить сессию БД."""
-    async with db_manager.session() as session:
-        yield session
+def get_uow() -> IUnitOfWork:
+    return SqlAlchemyUnitOfWork(db_manager)
 
 
-async def get_events_service(
-    session: Annotated[AsyncSession, Depends(get_session)],
+def get_events_provider_client() -> IEventsProviderClient:
+    return EventsProviderClient()
+
+
+def get_events_service(
+    uow: Annotated[IUnitOfWork, Depends(get_uow)],
+    client: Annotated[
+        IEventsProviderClient, Depends(get_events_provider_client)
+    ],
 ) -> EventsService:
-    """Получить сервис событий."""
-    return EventsService(session, EventsProviderClient())
+    return EventsService(uow, client)
 
 
-async def get_tickets_service(
-    session: Annotated[AsyncSession, Depends(get_session)],
+def get_tickets_service(
+    uow: Annotated[IUnitOfWork, Depends(get_uow)],
+    client: Annotated[
+        IEventsProviderClient, Depends(get_events_provider_client)
+    ],
 ) -> TicketsService:
-    """Получить сервис регистрации."""
-    return TicketsService(session, EventsProviderClient())
+    return TicketsService(uow, client)
